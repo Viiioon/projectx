b6102b865271a09e4cb8327479f0540270fac2bb
Viiioon <vionhasaj@gmail.com>
2023-06-08 19:40:46 +0200

    broken gui but hey at least it works

--- projectx/src/main/app/script.js
+++ projectx/src/main/app/script.js
@@ -12,24 +12,81 @@ function getData() {
       });
   }
   
-  // Function to display the retrieved data in the container
+  // Function to display the retrieved data in the table
   function displayData(data) {
-    const dataContainer = document.getElementById('dataContainer');
-    dataContainer.innerHTML = '';
+    const dataTable = document.getElementById('data-table');
+    const tbody = dataTable.querySelector('tbody');
+    tbody.innerHTML = '';
   
     if (data && data.length > 0) {
+      const idFilter = document.getElementById('idFilter').value.trim();
+      const areaFilter = document.getElementById('areaFilter').value.trim();
+  
       data.forEach(item => {
-        const p = document.createElement('p');
-        p.textContent = item.name;
-        dataContainer.appendChild(p);
+        if ((idFilter && !item.id.toString().includes(idFilter))
+          || (areaFilter && !item.areaOfStudy.toLowerCase().includes(areaFilter.toLowerCase()))) {
+          return; // Skip if ID or Area of Study does not match the filters
+        }
+  
+        const row = document.createElement('tr');
+        const idCell = document.createElement('td');
+        idCell.textContent = item.id;
+        const nameCell = document.createElement('td');
+        nameCell.textContent = item.name;
+        const areaCell = document.createElement('td');
+        areaCell.textContent = item.areaOfStudy;
+        row.appendChild(idCell);
+        row.appendChild(nameCell);
+        row.appendChild(areaCell);
+        tbody.appendChild(row);
       });
     } else {
-      const p = document.createElement('p');
-      p.textContent = 'No data available';
-      dataContainer.appendChild(p);
+      const row = document.createElement('tr');
+      const messageCell = document.createElement('td');
+      messageCell.setAttribute('colspan', '3');
+      messageCell.textContent = 'No data available';
+      row.appendChild(messageCell);
+      tbody.appendChild(row);
     }
   }
+  
+  
+  
+  
+  
+  function filterData() {
+    const idFilter = document.getElementById('idFilter').value.toLowerCase();
+    const nameFilter = document.getElementById('nameFilter').value.toLowerCase();
+    const areaFilter = document.getElementById('areaFilter').value.toLowerCase();
+    const tableBody = document.getElementById('data-table').getElementsByTagName('tbody')[0];
+    const rows = tableBody.getElementsByTagName('tr');
+  
+    for (let i = 0; i < rows.length; i++) {
+      const idCell = rows[i].getElementsByTagName('td')[0];
+      const nameCell = rows[i].getElementsByTagName('td')[1];
+      const areaOfStudyCell = rows[i].getElementsByTagName('td')[2];
+  
+      const idValue = idCell.textContent || idCell.innerText;
+      const nameValue = nameCell.textContent || nameCell.innerText;
+      const areaOfStudyValue = areaOfStudyCell.textContent || areaOfStudyCell.innerText;
+
+  
+      if (idValue.toLowerCase().indexOf(idFilter) > -1 && nameValue.toLowerCase().indexOf(nameFilter) > -1 && areaOfStudyValue.toLowerCase().indexOf(areaFilter)) {
+        rows[i].style.display = '';
+      } else {
+        rows[i].style.display = 'none';
+      }
+    }
+  }
+  
+  // Add event listeners for the filter inputs
+  document.getElementById('idFilter').addEventListener('input', filterData);
+  document.getElementById('nameFilter').addEventListener('input', filterData);
+  document.getElementById('areaFilter').addEventListener('input', filterData);
+
+  
   
   // Add an event listener to the button
   const getDataButton = document.getElementById('getDataButton');
   getDataButton.addEventListener('click', getData);
+  
