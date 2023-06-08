82225db2890b65c7676391c8cdf227abed3ac238
Viiioon <vionhasaj@gmail.com>
2023-06-08 10:54:49 +0200

    removed hello world statement

--- projectx/src/main/java/ch/zhaw/projectx/ProjectxApplication.java
+++ projectx/src/main/java/ch/zhaw/projectx/ProjectxApplication.java
@@ -11,7 +11,6 @@ public class ProjectxApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectxApplication.class, args);
-		System.out.println("hello world");
 	}
 
 }
