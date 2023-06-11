cf9399c1a11761471bc7f571b752e352d5bdd907
Viiioon <vionhasaj@gmail.com>
2023-06-11 22:47:59 +0200

    small refactoring

--- projectx/src/main/java/ch/zhaw/projectx/restcontroller/ExplanationController.java
+++ projectx/src/main/java/ch/zhaw/projectx/restcontroller/ExplanationController.java
@@ -1,15 +1,11 @@
 package ch.zhaw.projectx.restcontroller;
 
-import ch.zhaw.projectx.entity.Domain;
 import ch.zhaw.projectx.entity.Explanation;
 import ch.zhaw.projectx.repository.ExplanationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
