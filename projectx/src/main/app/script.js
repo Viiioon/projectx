a7da79fd28b54c171c7427e9596517fcea40cc33
Viiioon <vionhasaj@gmail.com>
2023-06-11 23:25:18 +0200

    small refactoring

--- projectx/src/main/app/script.js
+++ projectx/src/main/app/script.js
@@ -1,30 +1,61 @@
-window.onload = function() {
-  // Function to handle the loading event
-  fetch('http://localhost:8080/api/domains')
-    .then(response => response.json())
-    .then(data => {
-      // Handle the retrieved data
-      displayData(data);
-    })
-    .catch(error => {
-      console.error('Error retrieving data:', error);
-    });
+let originalData = [];
+
+window.onload = async function () {
+  try {
+    // Load data from API
+    const response = await fetch("http://localhost:8080/api/domains");
+    const data = await response.json();
+
+    // Save original data for searching
+    originalData = data;
+    displayData(data);
+  } catch (error) {
+    console.error("Error retrieving data:", error);
+  }
+
+  // Add event listeners for search fields
+  document.getElementById("id-input").addEventListener("keyup", (event) => {
+    searchAndUpdateDisplay(Number(event.target.value), "id", true);
+  });
+
+  document.getElementById("name-input").addEventListener("keyup", (event) => {
+    searchAndUpdateDisplay(event.target.value, "name");
+  });
+
+  document.getElementById("area-input").addEventListener("keyup", (event) => {
+    searchAndUpdateDisplay(event.target.value, "areaOfStudy");
+  });
 };
+
+// Function to perform a search and update the display
+function searchAndUpdateDisplay(query, field, isExactMatch = false) {
+  const filteredData = originalData.filter((item) => {
+    // If exact match is requested, return true only if field is exactly equal to query.
+    if (isExactMatch) {
+      return item[field] === query;
+    } else {
+      // Otherwise, return true if field contains query (case-insensitive).
+      return item[field].toString().toLowerCase().includes(query.toLowerCase());
+    }
+  });
+
+  displayData(filteredData);
+}
 
 // Function to display the retrieved data in the table
 function displayData(data) {
-  const dataTable = document.getElementById('data-table');
-  const tbody = dataTable.querySelector('tbody');
-  tbody.innerHTML = '';
+  const dataTable = document.getElementById("data-table");
+  const tbody = dataTable.querySelector("tbody");
+  tbody.innerHTML = "";
 
   if (data && data.length > 0) {
-    data.forEach(item => {
-      const row = document.createElement('tr');
-      const idCell = document.createElement('td');
+    data.forEach((item) => {
+      const row = document.createElement("tr");
+      const idCell = document.createElement("td");
       idCell.textContent = item.id;
-      const nameCell = document.createElement('td');
+      const nameCell = document.createElement("td");
       nameCell.textContent = item.name;
-      const areaCell = document.createElement('td');
+      const areaCell = document.createElement("td");
       areaCell.textContent = item.areaOfStudy;
       row.appendChild(idCell);
       row.appendChild(nameCell);
@@ -32,10 +63,10 @@ function displayData(data) {
       tbody.appendChild(row);
     });
   } else {
-    const row = document.createElement('tr');
-    const messageCell = document.createElement('td');
-    messageCell.setAttribute('colspan', '3');
-    messageCell.textContent = 'No data available';
+    const row = document.createElement("tr");
+    const messageCell = document.createElement("td");
+    messageCell.setAttribute("colspan", "3");
+    messageCell.textContent = "No data available";
     row.appendChild(messageCell);
     tbody.appendChild(row);
   }
